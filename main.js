3d86d3afcf47213d0960e22258bb45ea9f53f5ed
Robloxfortniterroblox <azpazou@gmail.com>
2026-01-24 09:18:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-const { autoUpdater } = require("electron-updater")
 const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater")
 const path = require('path')
 
-// 1. LIVE RELOAD (Add this for optimization)
 try {
   require('electron-reloader')(module)
 } catch (_) {}
 
 function createWindow () {
   const win = new BrowserWindow({
-    // Start hidden so the user doesn't see it resize
     show: false, 
     
-    // OPTION A: Fullscreen (Game Mode - No borders/taskbar)
-    // fullscreen: true, 
-    
-    // OPTION B: Standard Maximized Window (Remove comments to use)
     width: 1280,
     height: 720,
+
+    icon: path.join(__dirname, 'icon.ico'), 
 
     webPreferences: {
       nodeIntegration: true,
@@ -26,20 +22,12 @@ function createWindow () {
     autoHideMenuBar: true
   })
 
-  // Load the file
-  win.loadFile('index.html')
-
-  // 2. MAXIMIZE COMMAND
-  // If you are NOT using 'fullscreen: true', uncomment the line below:
+win.loadFile('index.html')
   win.maximize()
 
-  // Show the window only after it is fully loaded and maximized
   win.once('ready-to-show', () => {
     win.show()
-    win.once('ready-to-show', () => {
-    win.show()
-    autoUpdater.checkForUpdatesAndNotify() // <--- Checks for update on startup
-  })
+    autoUpdater.checkForUpdatesAndNotify() 
   })
 }
 
@@ -58,12 +46,11 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
 autoUpdater.on('update-available', () => {
-  // You can show a dialog here saying "Downloading update..."
   console.log('Update available')
 })
 
 autoUpdater.on('update-downloaded', () => {
-  // Silent install and restart
   autoUpdater.quitAndInstall()
 })
